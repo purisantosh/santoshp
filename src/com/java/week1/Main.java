061685a33c1f9af5caa43a8af6892a780f910fbc
purisantosh <116016459+purisantosh@users.noreply.github.com>
2023-06-05 15:38:57 -0500

    commit

--- src/com/java/week1/Main.java
+++ src/com/java/week1/Main.java
@@ -1,6 +1,12 @@
 public class Main{
     public static void main(String args[])
     {
+        float a=10;
+        a=20;
+        System.out.println(a);
         System.out.println("Hello World!");
+
+
+
     }
 }
